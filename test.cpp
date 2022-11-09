c19c28f1c136c0945fa93d9bab90d03d9e538bf4
yogi <deschamps.yogi@gmail.com>
2022-11-09 22:09:22 +0000

    ajout de l'utilisation de la lib boost ajout des outils gcov lcov ....

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include "boost/format.hpp"
 
 using namespace std; 
 
@@ -11,4 +12,8 @@ int main (int argc, char** argv){
         std::cout << "Your PATH is: " << env_p << '\n';
     else
         std::cout << "Attention ma variable MY_VAR n'est pas définie" << std::endl;
+    
+    std::string users[] = {"joe", "bill", "bob"};
+    for (int i = 0 ; i < 3 ; i++)
+        std::cout << "  " << (boost::format("Bonjour utilisateur n°%1% nommé %2%.") % i % users[i]).str() << std::endl;
 }
